9b3d940993cdb26cbf86b3b138facff889d04958
HarveyGW <45942623+HarveyGW@users.noreply.github.com>
2019-03-07 21:30:56 +0000

    Create InsertionSort.py

---
+++ InsertionSort.py
@@ -0,0 +1,20 @@
+import time
+from time import sleep
+def InsertionSort(Arrlist):
+  for index in range(1, len(Arrlist)):
+    value = Arrlist[index]
+    i = index - 1
+    while i >= 0:
+      if value < Arrlist[i]:
+        Arrlist[i + 1] = Arrlist[i] #Shift number in slot i right to slot i+1
+        Arrlist [i] = value #Shift value left into slot i
+        i -= 1
+        print(Arrlist)
+        time.sleep(0.1)
+      else:
+        break
+
+Arrlist = input("Enter the numbers: ").split()
+InsertionSort(Arrlist)
+
+ 
